43014da075fc6291fccdcd7ae4c8bd81eb2703ff
Bianca Gibson <bianca.gibson@bom.gov.au>
2019-05-31 18:27:47 +1000

    Add conda packaging

--- packages/setup.py
+++ packages/setup.py
@@ -1,18 +1,17 @@
-from setuptools import setup,find_packages
+from setuptools import setup
 
 setup(name='awrams',
-      packages=['awrams', 'awrams.benchmarking', 'awrams.calibration',
-                'awrams.cluster','awrams.models', 'awrams.simulation',
-                'awrams.utils','awrams.visualisation'],
+      packages=['awrams'],
       version='1.2',
       description='AWRA Modelling System (Community Release), version 1.2',
       url='https://github.com/awracms/awra_cms',
       author='awrams team',
       author_email='awrams@bom.gov.au',
-      license='MIT',
+      license='AWRA License',
       zip_safe=False,
       include_package_data=True,
       setup_requires=['nose>=1.3.3'],
       test_suite='nose.collector',
       tests_require=['nose'],
-      )
+      python_requires='==3.6.*',
+)
